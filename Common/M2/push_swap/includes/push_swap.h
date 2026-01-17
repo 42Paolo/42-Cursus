f958566c32b4d9a076f6dc614094be97e178d46d
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-01-17 16:28:17 +0100

    fattop

---
+++ Common/M2/push_swap/includes/push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabrogi <pabrogi@student.42firenze.it>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/17 16:14:43 by pabrogi           #+#    #+#             */
+/*   Updated: 2026/01/17 16:21:20 by pabrogi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+
+# include "stack.h"
+# include "operations.h"
+# include "parser.h"
+# include "solver.h"
+# include "utils.h"
+
+#endif
