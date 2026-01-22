5f27daf597a0e342203d545e0a324af72a292f50
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-01-22 13:17:14 +0100

    ex3

---
+++ Common/M3/PY00/ex3/ft_plant_age.py
@@ -0,0 +1,12 @@
+def ft_plant_age():
+	try:
+		age_in_days = int(input("Enter plant age in days: "))
+		if age_in_days < 0:
+			print("Plant age in days can't be negative")
+			return	
+		elif age_in_days > 60:
+			print("Plant is ready to harvest!")
+		else:
+			print("Plant needs more time to grow.")
+	except ValueError:
+		print("Invalid input! Please enter an integer number.")
