f769cffb73a9678021d630445cbf7cf759c93610
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-01-22 12:19:49 +0100

    add: subjects giusti

--- Common/M3/PY00/ex01/format_ft_time.py
+++
@@ -1,8 +0,0 @@
-import time
-from datetime import datetime
-
-date_today = datetime.now()
-epoch_seconds = time.time()
-
-print("Seconds since January 1, 1970:", f"{epoch_seconds:,.4f}", "or", f"{epoch_seconds:.2e}", "in scientific notation")
-print(date_today.strftime("%b %d %Y")) 
