86713c0ecbe21c8c842e94d2e00ea1394cb4d5b9
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-01-22 13:23:01 +0100

    ex4

---
+++ Common/M3/PY00/ex4/ft_water_reminder.py
@@ -0,0 +1,12 @@
+def ft_water_reminder():
+	try:
+		last_watering = int(input("Days since last watering: "))
+		if last_watering < 0:
+			print("The number of days since the last watering can't be negative")
+			return
+		elif last_watering > 2:
+			print("Water the plants")
+		else:
+			print("Plants are fine")
+	except ValueError:
+		print("Invalid input! Please enter an integer number.")
