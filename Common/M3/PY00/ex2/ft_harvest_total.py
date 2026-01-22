b91fcc64b8987a1977ac9ebe1fdeb7b655d206ca
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-01-22 13:10:53 +0100

    add: ex2

---
+++ Common/M3/PY00/ex2/ft_harvest_total.py
@@ -0,0 +1,11 @@
+def ft_harvest_total():
+	try:
+		d1 = int(input("Day 1 harvest: "))
+		d2 = int(input("Day 2 harvest: "))
+		d3 = int(input("Day 3 harvest: "))
+		if d1 < 0 or d2 < 0 or d3 < 0:
+			print("Harvest can't be negative")
+			return
+		print(f"Total harvest: {d1 + d2 + d3}")
+	except ValueError:
+		 print("Invalid input! Please enter an integer number.")
