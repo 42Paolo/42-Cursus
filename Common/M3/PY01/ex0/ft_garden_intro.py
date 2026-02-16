44f3ea68885dbefe9b859d90f7ac963fe2d66dc8
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-02-16 14:32:15 +0100

    fatt

--- Common/M3/PY01/ex0/ft_garden_intro.py
+++ Common/M3/PY01/ex0/ft_garden_intro.py
@@ -1,4 +1,25 @@
+class Plant:
+	def __init__(self, name, height_cm, age_d):
+		self.name = name
+		self.height_cm = height_cm
+		self.age_d = age_d
+		
+	def print_info(self):
+		print(f"Plant: {self.name}\nHeight: {self.height_cm}cm\nAge: {self.age_d}\n")
+
+def main(): 
+	plants = [
+		Plant("Rose", 25, 30),
+		Plant("Tulipano", 40, 10),
+		Plant("Eocalipto", 40, 15)
+	]
+	
+	print("=== Welcome to My Garden ===")
+	for p in plants:
+		p.print_info()
+
+	print("=== End of Program ===")
+
+
 if __name__ == "__main__":
-    print("=== Welcome to My Garden ===")
-    print("Plant: Rose\nHeight: 25cm\nAge: 30 days\n")
-    print("=== End of Program ===")
+	main()
