71d81f7402248bdbc654aed2505c29a2b06dae09
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-02-18 15:42:25 +0100

    fatto

--- Common/M3/PY03/ex0/ft_command_quest.py
+++ Common/M3/PY03/ex0/ft_command_quest.py
@@ -1,14 +1,22 @@
 import sys
 
 def ft_len(obj):
-    count = 0
-    for _ in obj:
-        count += 1
-    return count
+	count = 0
+	for _ in obj:
+		count += 1
+	return count
 
 def main():
 	print("=== Command Quest ===")
-
+	argc = ft_len(sys.argv)
+	if argc == 1:
+		print("No arguments provided!")
+	print(f"Program name: {sys.argv[0]}")		
+	if argc != 1:
+		print(f"Arguments received: {argc-1}")
+		for i in range(1, argc):
+			print(f"Argument {i}: {sys.argv[i]}")
+	print(f"Total arguments: {argc}")
 
 if __name__ == "__main__":
 	main()
