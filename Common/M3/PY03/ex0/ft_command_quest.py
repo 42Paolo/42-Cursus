a8c96bd8d3940435c3b53d8739c03f03bccd3695
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-02-18 15:08:55 +0100

    fatto

---
+++ Common/M3/PY03/ex0/ft_command_quest.py
@@ -0,0 +1,14 @@
+import sys
+
+def ft_len(obj):
+    count = 0
+    for _ in obj:
+        count += 1
+    return count
+
+def main():
+	print("=== Command Quest ===")
+
+
+if __name__ == "__main__":
+	main()
